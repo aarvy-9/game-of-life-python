bff8d3e098435eae9bcc25e1fa2ef3a85bff5449
hariprasad.jella05@gmail.com <hariprasad.jella05@gmail.com@1e9af996-6bc9-4e4b-9a57-10da00564793>
2019-11-02 06:15:07 +0000

    Added non-mutable version
    git-svn-id: https://agile.cs.uh.edu/svn/f19_proglangs/jella_vempati/assign4/python@2714 1e9af996-6bc9-4e4b-9a57-10da00564793

--- src/game_of_life.py
+++ src/game_of_life.py
@@ -1,2 +1,35 @@
-def game_of_life():
-  return True
+def neighbour_count(initial_state, row, col, cell_type):
+
+  # cell count mutated in this function
+
+  neighbours = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+  cell_count = 0
+
+  for neighbour in neighbours:
+    if not (row + neighbour[0] < 0 or col + neighbour[1] < 0 or row + neighbour[0] > len(initial_state) - 1 or col + neighbour[1] > len(initial_state[0]) - 1):
+      if initial_state[row + neighbour[0]][col + neighbour[1]] == 1 and cell_type == 1:
+        cell_count += 1
+      elif initial_state[row + neighbour[0]][col + neighbour[1]] == 1 and cell_type == 0:
+        cell_count += 1
+    
+  return cell_count
+
+
+def game_of_life(initial_state):
+
+  # next_generation mutated in this function:
+  # For now created only for 1 next generation. can extend it to multiple geneations using a while loop
+
+  next_generation = [[0 for col in range(len(initial_state[0]))] for row in range(len(initial_state))]
+  
+  for i in range(len(initial_state)):
+    for j in range(len(initial_state[0])):
+      if initial_state[i][j] == 1 and 2 <= neighbour_count(initial_state, i, j, 1) <= 3:
+          next_generation[i][j] = 1
+      elif neighbour_count(initial_state, i, j, 0) == 3:
+          next_generation[i][j] = 1
+
+  return next_generation
+
+initial_state = [[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]]
+print(game_of_life(initial_state))
--- test/game_of_life_test.py
+++ test/game_of_life_test.py
@@ -4,4 +4,5 @@ from src import game_of_life
 class GameOfLifeTest(unittest.TestCase):
 
   def test_game_of_life(self):
-    self.assertTrue(game_of_life.game_of_life())
+    initial_state = [[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 1, 1, 0]]
+    self.assertTrue(game_of_life.game_of_life(initial_state))
