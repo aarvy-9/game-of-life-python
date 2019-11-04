c0399a338376d915efc2cf959a25cdf298ca7d3c
hariprasad.jella05@gmail.com <hariprasad.jella05@gmail.com@1e9af996-6bc9-4e4b-9a57-10da00564793>
2019-11-04 04:13:19 +0000

    Modified code as per comments
    git-svn-id: https://agile.cs.uh.edu/svn/f19_proglangs/jella_vempati/assign4/python@2766 1e9af996-6bc9-4e4b-9a57-10da00564793

--- src/game_of_life.py
+++ src/game_of_life.py
@@ -1,30 +1,10 @@
-def increment_count(count):
-  return count + 1
-
-def neighbour_count(initial_state, row, col, cell_type):
-
-  neighbours = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
-  cell_count = 0
-
-  for neighbour in neighbours:
-    if not (row + neighbour[0] < 0 or col + neighbour[1] < 0 or row + neighbour[0] > len(initial_state) - 1 or col + neighbour[1] > len(initial_state[0]) - 1):
-      if initial_state[row + neighbour[0]][col + neighbour[1]] == 1 and cell_type == 1:
-        cell_count = increment_count(cell_count)
-      elif initial_state[row + neighbour[0]][col + neighbour[1]] == 1 and cell_type == 0:
-        cell_count = increment_count(cell_count)
+def is_alive(current_state, number_of_live_neighbors):
   
-  return cell_count
-
-
-def game_of_life(initial_state):
-
-  next_generation = [[0 for col in range(len(initial_state[0]))] for row in range(len(initial_state))]
+  if current_state == 1:
+    if number_of_live_neighbors == 2 or number_of_live_neighbors == 3:
+      return True
+  else:
+    if number_of_live_neighbors == 3:
+      return True
   
-  for i in range(len(initial_state)):
-    for j in range(len(initial_state[0])):
-      if initial_state[i][j] == 1 and 2 <= neighbour_count(initial_state, i, j, 1) <= 3:
-          next_generation[i][j] = 1
-      elif neighbour_count(initial_state, i, j, 0) == 3:
-          next_generation[i][j] = 1
-
-  return next_generation
+  return False
