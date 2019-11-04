c2363c28daff52a56a207638e358347f5ed0121e
hariprasad.jella05@gmail.com <hariprasad.jella05@gmail.com@1e9af996-6bc9-4e4b-9a57-10da00564793>
2019-11-04 23:42:57 +0000

    Modified Test Cases
    git-svn-id: https://agile.cs.uh.edu/svn/f19_proglangs/jella_vempati/assign4/python@2788 1e9af996-6bc9-4e4b-9a57-10da00564793

--- test/game_of_life_test.py
+++ test/game_of_life_test.py
@@ -7,15 +7,15 @@ class GameOfLifeTest(unittest.TestCase):
     self.assertTrue(True, game_of_life.is_alive(1, 2))
 
   def test_game_of_life_live_cell_neighbours_1(self):
-    self.assertTrue(True, game_of_life.is_alive(1, 1))
+    self.assertFalse(False, game_of_life.is_alive(1, 1))
 
   def test_game_of_life_live_cell_neighbours_4(self):
-    self.assertTrue(True, game_of_life.is_alive(1, 4))
+    self.assertFalse(False, game_of_life.is_alive(1, 4))
 
   def test_game_of_life_dead_cell_neighbours_3(self):
     self.assertTrue(True, game_of_life.is_alive(0, 3))
 
   def test_game_of_life_dead_cell_neighbours_2(self):
-    self.assertTrue(True, game_of_life.is_alive(0, 2))
+    self.assertFalse(False, game_of_life.is_alive(0, 2))
 
 #Feedback: tests for when a cell would die
