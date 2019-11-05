d7de68154996b28c75115bc2372c2f0f9f54b5ea
hariprasad.jella05@gmail.com <hariprasad.jella05@gmail.com@1e9af996-6bc9-4e4b-9a57-10da00564793>
2019-11-05 00:17:29 +0000

    Updated Test cases
    git-svn-id: https://agile.cs.uh.edu/svn/f19_proglangs/jella_vempati/assign4/python@2789 1e9af996-6bc9-4e4b-9a57-10da00564793

--- test/game_of_life_test.py
+++ test/game_of_life_test.py
@@ -2,20 +2,38 @@ import unittest
 from src import game_of_life
 
 class GameOfLifeTest(unittest.TestCase):
-
-  def test_game_of_life_live_cell_neighbours_2(self):
-    self.assertTrue(True, game_of_life.is_alive(1, 2))
+  def test_game_of_life_live_cell_neighbours_0(self):
+    self.assertFalse(game_of_life.is_alive(1, 0))
 
   def test_game_of_life_live_cell_neighbours_1(self):
-    self.assertFalse(False, game_of_life.is_alive(1, 1))
+    self.assertFalse(game_of_life.is_alive(1, 1))
+  
+  def test_game_of_life_live_cell_neighbours_2(self):
+    self.assertTrue(game_of_life.is_alive(1, 2))
+
+  def test_game_of_life_live_cell_neighbours_3(self):
+    self.assertTrue(game_of_life.is_alive(1, 3))
 
   def test_game_of_life_live_cell_neighbours_4(self):
-    self.assertFalse(False, game_of_life.is_alive(1, 4))
+    self.assertFalse(game_of_life.is_alive(1, 4))
 
-  def test_game_of_life_dead_cell_neighbours_3(self):
-    self.assertTrue(True, game_of_life.is_alive(0, 3))
+  def test_game_of_life_live_cell_neighbours_5(self):
+    self.assertFalse(game_of_life.is_alive(1, 5))
+
+  def test_game_of_life_dead_cell_neighbours_0(self):
+    self.assertFalse(game_of_life.is_alive(0, 0))
+
+  def test_game_of_life_dead_cell_neighbours_1(self):
+    self.assertFalse(game_of_life.is_alive(0, 1))
 
   def test_game_of_life_dead_cell_neighbours_2(self):
-    self.assertFalse(False, game_of_life.is_alive(0, 2))
+    self.assertFalse(game_of_life.is_alive(0, 2))
 
-#Feedback: tests for when a cell would die
+  def test_game_of_life_dead_cell_neighbours_3(self):
+    self.assertTrue(game_of_life.is_alive(0, 3))
+
+  def test_game_of_life_dead_cell_neighbours_4(self):
+    self.assertFalse(game_of_life.is_alive(0, 4))
+
+  def test_game_of_life_dead_cell_neighbours_5(self):
+    self.assertFalse(game_of_life.is_alive(0, 5))
