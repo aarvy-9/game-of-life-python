44e0a5234c0658e5c3186da03950a0a43f1eb359
rahulv2901@gmail.com <rahulv2901@gmail.com@1e9af996-6bc9-4e4b-9a57-10da00564793>
2019-11-06 00:43:27 +0000

    Updated as per feedback
    git-svn-id: https://agile.cs.uh.edu/svn/f19_proglangs/jella_vempati/assign4/python@2814 1e9af996-6bc9-4e4b-9a57-10da00564793

--- src/game_of_life.py
+++ src/game_of_life.py
@@ -1,12 +1,12 @@
 def is_alive(current_state, number_of_live_neighbors):
   if current_state == 1:
     if number_of_live_neighbors == 2 or number_of_live_neighbors == 3:
-      return True
+      return current_state, number_of_live_neighbors
   else:
     if number_of_live_neighbors == 3:
-      return True
+      return 1, number_of_live_neighbors
   
-  return False
+  return 0, number_of_live_neighbors
 
 #  return current_state and number_of_live_neighbors == 2 or \
 #    number_of_live_neighbors == 3
