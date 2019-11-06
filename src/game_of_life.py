59c771ac48095271385271585935e84ab1557629
rahulv2901@gmail.com <rahulv2901@gmail.com@1e9af996-6bc9-4e4b-9a57-10da00564793>
2019-11-06 17:07:16 +0000

    return type updated
    git-svn-id: https://agile.cs.uh.edu/svn/f19_proglangs/jella_vempati/assign4/python@2841 1e9af996-6bc9-4e4b-9a57-10da00564793

--- src/game_of_life.py
+++ src/game_of_life.py
@@ -1,11 +1,11 @@
 def is_alive(current_state, number_of_live_neighbors):
   if current_state:
     if number_of_live_neighbors == 2 or number_of_live_neighbors == 3:
-      return [True, number_of_live_neighbors]
+      return True
   else:
     if number_of_live_neighbors == 3:
-      return [True, number_of_live_neighbors]
+      return True
   
-  return [False, number_of_live_neighbors]
+  return False
 
 #Feedback: return True or return False, no need to return number_of_live_neighbors
--- test/game_of_life_test.py
+++ test/game_of_life_test.py
@@ -3,37 +3,37 @@ from src import game_of_life
 
 class GameOfLifeTest(unittest.TestCase):
   def test_game_of_life_live_cell_neighbours_0(self):
-    self.assertEqual([False, 0], game_of_life.is_alive(True, 0))
+    self.assertEqual(False, game_of_life.is_alive(True, 0))
 
   def test_game_of_life_live_cell_neighbours_1(self):
-    self.assertEqual([False, 1], game_of_life.is_alive(True, 1))
+    self.assertEqual(False, game_of_life.is_alive(True, 1))
   
   def test_game_of_life_live_cell_neighbours_2(self):
-    self.assertEqual([True, 2], game_of_life.is_alive(True, 2)) 
+    self.assertEqual(True, game_of_life.is_alive(True, 2)) 
 
   def test_game_of_life_live_cell_neighbours_3(self):
-    self.assertEqual([True, 3], game_of_life.is_alive(True, 3)) 
+    self.assertEqual(True, game_of_life.is_alive(True, 3)) 
 
   def test_game_of_life_live_cell_neighbours_4(self):
-    self.assertEqual([False, 4], game_of_life.is_alive(True, 4))
+    self.assertEqual(False, game_of_life.is_alive(True, 4))
 
   def test_game_of_life_live_cell_neighbours_5(self):
-    self.assertEqual([False, 5], game_of_life.is_alive(True, 5))
+    self.assertEqual(False, game_of_life.is_alive(True, 5))
 
   def test_game_of_life_dead_cell_neighbours_0(self):
-    self.assertEqual([False, 0], game_of_life.is_alive(False, 0))
+    self.assertEqual(False, game_of_life.is_alive(False, 0))
 
   def test_game_of_life_dead_cell_neighbours_1(self):
-    self.assertEqual([False, 1], game_of_life.is_alive(False, 1))
+    self.assertEqual(False, game_of_life.is_alive(False, 1))
 
   def test_game_of_life_dead_cell_neighbours_2(self):
-    self.assertEqual([False, 2], game_of_life.is_alive(False, 2))
+    self.assertEqual(False, game_of_life.is_alive(False, 2))
 
   def test_game_of_life_dead_cell_neighbours_3(self):
-    self.assertEqual([True, 3], game_of_life.is_alive(False, 3))
+    self.assertEqual(True, game_of_life.is_alive(False, 3))
 
   def test_game_of_life_dead_cell_neighbours_4(self):
-    self.assertEqual([False, 4], game_of_life.is_alive(False, 4))
+    self.assertEqual(False, game_of_life.is_alive(False, 4))
 
   def test_game_of_life_dead_cell_neighbours_5(self):
-    self.assertEqual([False, 5], game_of_life.is_alive(False, 5))
+    self.assertEqual(False, game_of_life.is_alive(False, 5))
