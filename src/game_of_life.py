9259adec5cd630e859068e6fcb4dbc9e921ab8d9
hariprasad.jella05@gmail.com <hariprasad.jella05@gmail.com@1e9af996-6bc9-4e4b-9a57-10da00564793>
2019-11-08 07:14:03 +0000

    Added next small step
    git-svn-id: https://agile.cs.uh.edu/svn/f19_proglangs/jella_vempati/assign4/python@2902 1e9af996-6bc9-4e4b-9a57-10da00564793

--- src/game_of_life.py
+++ src/game_of_life.py
@@ -1,7 +1,10 @@
-def is_alive(current_state, number_of_live_neighbors):
-  return (number_of_live_neighbors == 2 and current_state) or number_of_live_neighbors == 3 #Feedback: code runs off and wraps.
+def generate_signals(index):
+  return [(index[0] - 1, index[1] - 1), (index[0] - 1, index[1]), (index[0] - 1, index[1] + 1), (index[0], index[1] - 1),
+          (index[0], index[1] + 1), (index[0] + 1, index[1] - 1), (index[0] + 1, index[1]), (index[0] + 1, index[1] + 1)]
 
-def generate_signals(pos): #Feedback: let's take in a tuple instead of two parameters
-  i = pos[0]
-  j = pos[1]
-  return [(i - 1, j - 1), (i, j - 1), (i + 1, j - 1), (i + 1, j), (i + 1, j + 1), (i, j + 1), (i - 1, j), (i - 1, j - 1)] #Feedback: space arond operators like -
+def get_live_neighbours_count(universe, index):
+  return len(list(filter(lambda x: universe[x[0]][x[1]] == 1, filter(lambda x: -1 not in x and (x[0] < len(universe)) and (x[1] < len(universe[0])), generate_signals(index)))))
+
+def is_alive(universe, index):
+  number_of_live_neighbors = get_live_neighbours_count(universe, index)
+  return (number_of_live_neighbors == 2 and universe[index[0]][index[1]]) or number_of_live_neighbors == 3
--- test/game_of_life_test.py
+++ test/game_of_life_test.py
@@ -2,48 +2,62 @@ import unittest
 from src import game_of_life
 
 class GameOfLifeTest(unittest.TestCase):
-  def test_game_of_life_live_cell_neighbours_0(self):
-    self.assertEqual(False, game_of_life.is_alive(True, 0))
+  # def test_game_of_life_live_cell_neighbours_0(self):
+  #   self.assertEqual(False, game_of_life.is_alive(True, 0))
 
-  def test_game_of_life_live_cell_neighbours_1(self):
-    self.assertEqual(False, game_of_life.is_alive(True, 1))
+  # def test_game_of_life_live_cell_neighbours_1(self):
+  #   self.assertEqual(False, game_of_life.is_alive(True, 1))
   
-  def test_game_of_life_live_cell_neighbours_2(self):
-    self.assertEqual(True, game_of_life.is_alive(True, 2)) 
+  # def test_game_of_life_live_cell_neighbours_2(self):
+  #   self.assertEqual(True, game_of_life.is_alive(True, 2)) 
 
-  def test_game_of_life_live_cell_neighbours_3(self):
-    self.assertEqual(True, game_of_life.is_alive(True, 3)) 
+  # def test_game_of_life_live_cell_neighbours_3(self):
+  #   self.assertEqual(True, game_of_life.is_alive(True, 3)) 
 
-  def test_game_of_life_live_cell_neighbours_4(self):
-    self.assertEqual(False, game_of_life.is_alive(True, 4))
+  # def test_game_of_life_live_cell_neighbours_4(self):
+  #   self.assertEqual(False, game_of_life.is_alive(True, 4))
 
-  def test_game_of_life_live_cell_neighbours_5(self):
-    self.assertEqual(False, game_of_life.is_alive(True, 5))
+  # def test_game_of_life_live_cell_neighbours_5(self):
+  #   self.assertEqual(False, game_of_life.is_alive(True, 5))
 
-  def test_game_of_life_dead_cell_neighbours_0(self):
-    self.assertEqual(False, game_of_life.is_alive(False, 0))
+  # def test_game_of_life_dead_cell_neighbours_0(self):
+  #   self.assertEqual(False, game_of_life.is_alive(False, 0))
+
+  # def test_game_of_life_dead_cell_neighbours_1(self):
+  #   self.assertEqual(False, game_of_life.is_alive(False, 1))
+
+  # def test_game_of_life_dead_cell_neighbours_2(self):
+  #   self.assertEqual(False, game_of_life.is_alive(False, 2))
+
+  # def test_game_of_life_dead_cell_neighbours_3(self):
+  #   self.assertEqual(True, game_of_life.is_alive(False, 3))
+
+  # def test_game_of_life_dead_cell_neighbours_4(self):
+  #   self.assertEqual(False, game_of_life.is_alive(False, 4))
+
+  # def test_game_of_life_dead_cell_neighbours_5(self):
+  #   self.assertEqual(False, game_of_life.is_alive(False, 5))
+
+  
+#Feedback: code runs off and wraps	
+  # def test_generate_signals_2_3(self):
+  #   self.assertEqual([(1, 2), (1, 3), (1, 4), (2, 2), (2, 4), (3, 2), (3, 3), (3, 4)], game_of_life.generate_signals((2, 3)))
+	
+  # def test_generate_signals_1_4(self):
+  #   self.assertEqual([(0, 3), (0, 4), (0, 5), (1, 3), (1, 5), (2, 3), (2, 4), (2, 5)], game_of_life.generate_signals((1, 4)))
+	
+  # def test_generate_signals_0_0(self):
+  #   self.assertEqual([(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)], game_of_life.generate_signals((0, 0)))
+
 
   def test_game_of_life_dead_cell_neighbours_1(self):
-    self.assertEqual(False, game_of_life.is_alive(False, 1))
+    self.assertEqual(True, game_of_life.is_alive([[1, 1, 1], [1, 1, 1], [1, 0, 0]], (0, 0)))
 
   def test_game_of_life_dead_cell_neighbours_2(self):
-    self.assertEqual(False, game_of_life.is_alive(False, 2))
+    self.assertEqual(False, game_of_life.is_alive([[1, 1, 1], [1, 1, 1], [1, 0, 0]], (1, 0)))
 
   def test_game_of_life_dead_cell_neighbours_3(self):
-    self.assertEqual(True, game_of_life.is_alive(False, 3))
+    self.assertEqual(True, game_of_life.is_alive([[1, 1, 1], [1, 1, 0], [1, 0, 0]], (2, 1)))
 
   def test_game_of_life_dead_cell_neighbours_4(self):
-    self.assertEqual(False, game_of_life.is_alive(False, 4))
-
-  def test_game_of_life_dead_cell_neighbours_5(self):
-    self.assertEqual(False, game_of_life.is_alive(False, 5))
-
-#Feedback: code runs off and wraps	
-  def test_generate_signals_2_3(self):
-    self.assertEqual([(1, 2), (2, 2), (3, 2), (3, 3), (3, 4), (2, 4), (1, 3), (1, 2)], game_of_life.generate_signals((2, 3)))
-	
-  def test_generate_signals_1_4(self):
-    self.assertEqual([(0, 3), (1, 3), (2, 3), (2, 4), (2, 5), (1, 5), (0, 4), (0, 3)], game_of_life.generate_signals((1, 4)))
-	
-  def test_generate_signals_0_0(self):
-    self.assertEqual([(-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 0), (-1, -1)], game_of_life.generate_signals((0, 0)))
+    self.assertEqual(False, game_of_life.is_alive([[1, 1, 1], [1, 1, 0], [1, 0, 0]], (2, 2)))
