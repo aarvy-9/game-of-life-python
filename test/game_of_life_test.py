f1fb4cba1f56bead123f797e0c621abaad49c4f1
svenkat@cs.uh.edu <svenkat@cs.uh.edu@1e9af996-6bc9-4e4b-9a57-10da00564793>
2019-11-08 19:50:36 +0000

    assign4 synopsis:  not sure what's going on
    git-svn-id: https://agile.cs.uh.edu/svn/f19_proglangs/jella_vempati/assign4/python@2914 1e9af996-6bc9-4e4b-9a57-10da00564793

--- test/game_of_life_test.py
+++ test/game_of_life_test.py
@@ -1,6 +1,10 @@
 import unittest
 from src import game_of_life
 
+#Feedback: what happened to all the tests we wrote?!
+#why did we change the tests for is_alive from taking True, 0 to
+#this?
+
 class GameOfLifeTest(unittest.TestCase):
   def test_game_of_life_dead_cell_neighbours_1(self):
     self.assertEqual(True, game_of_life.is_alive([[1, 1, 1], [1, 1, 1], [1, 0, 0]], (0, 0)))
