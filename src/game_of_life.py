258e43b794c32ed8f88a4c72edb63e38b3628192
hariprasad.jella05@gmail.com <hariprasad.jella05@gmail.com@1e9af996-6bc9-4e4b-9a57-10da00564793>
2019-11-09 02:13:08 +0000

    Added Previous Test cases and also added main function
    git-svn-id: https://agile.cs.uh.edu/svn/f19_proglangs/jella_vempati/assign4/python@2923 1e9af996-6bc9-4e4b-9a57-10da00564793

--- src/game_of_life.py
+++ src/game_of_life.py
@@ -1,11 +1,17 @@
 #Feedback: code is wrapping on multiple lines making it hard to read.
 def generate_signals(index):
-  return [(index[0] - 1, index[1] - 1), (index[0] - 1, index[1]), (index[0] - 1, index[1] + 1), (index[0], index[1] - 1),
-          (index[0], index[1] + 1), (index[0] + 1, index[1] - 1), (index[0] + 1, index[1]), (index[0] + 1, index[1] + 1)]
+  return [(index[0] - 1, index[1] - 1), (index[0] - 1, index[1]), (index[0] - 1, index[1] + 1), (index[0], index[1] - 1), (index[0], index[1] + 1), (index[0] + 1, index[1] - 1), (index[0] + 1, index[1]), (index[0] + 1, index[1] + 1)]
 
 def get_live_neighbours_count(universe, index):
   return len(list(filter(lambda x: universe[x[0]][x[1]] == 1, filter(lambda x: -1 not in x and (x[0] < len(universe)) and (x[1] < len(universe[0])), generate_signals(index)))))
 
-def is_alive(universe, index):
-  number_of_live_neighbors = get_live_neighbours_count(universe, index)
-  return (number_of_live_neighbors == 2 and universe[index[0]][index[1]]) or number_of_live_neighbors == 3
+def is_alive(current_status, number_of_live_neighbors):
+  return (number_of_live_neighbors == 2 and current_status) or number_of_live_neighbors == 3
+
+def main_function(universe):
+  for i in range(len(universe)):
+    for j in range(len(universe[0])):
+      is_alive(universe[i][j], get_live_neighbours_count(universe, (i, j)))
+
+
+main_function([[True, True, True], [True, True, True], [True, False, False]])
