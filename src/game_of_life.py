2d7d2b92936ec6e1e3c57a67d136b55189d49bea
hariprasad.jella05@gmail.com <hariprasad.jella05@gmail.com@1e9af996-6bc9-4e4b-9a57-10da00564793>
2019-11-09 21:30:35 +0000

    Modified get_live_neighbours_count() function
    git-svn-id: https://agile.cs.uh.edu/svn/f19_proglangs/jella_vempati/assign4/python@2956 1e9af996-6bc9-4e4b-9a57-10da00564793

--- src/game_of_life.py
+++ src/game_of_life.py
@@ -1,8 +1,9 @@
-def generate_signals(index):
-  return [(index[0] - 1, index[1] - 1), (index[0] - 1, index[1]), (index[0] - 1, index[1] + 1), (index[0], index[1] - 1), (index[0], index[1] + 1), (index[0] + 1, index[1] - 1), (index[0] + 1, index[1]), (index[0] + 1, index[1] + 1)]
+def generate_signals(i):
+  return [(i[0] - 1, i[1] - 1), (i[0] - 1, i[1]), (i[0] - 1, i[1] + 1), (i[0], i[1] - 1), (i[0], i[1] + 1), (i[0] + 1, i[1] - 1), (i[0] + 1, i[1]), (i[0] + 1, i[1] + 1)]
 
 def get_live_neighbours_count(universe, index):
-  return len(list(filter(lambda x: universe[x[0]][x[1]] == 1, filter(lambda x: -1 not in x and (x[0] < len(universe)) and (x[1] < len(universe[0])), generate_signals(index)))))
+  valid_signals = filter(lambda x: -1 not in x and (x[0] < len(universe)) and (x[1] < len(universe[0])), generate_signals(index))
+  return len(list(filter(lambda x: universe[x[0]][x[1]] == 1, valid_signals)))
 
 def is_alive(current_status, number_of_live_neighbors):
   return (number_of_live_neighbors == 2 and current_status) or number_of_live_neighbors == 3
