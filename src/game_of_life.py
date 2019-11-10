a0f98c9cc6b7c4d6decddfedf4be0fe76c207124
rahulv2901@gmail.com <rahulv2901@gmail.com@1e9af996-6bc9-4e4b-9a57-10da00564793>
2019-11-10 06:54:06 +0000

    added generate_signals_for_all_live_cells method and test cases
    git-svn-id: https://agile.cs.uh.edu/svn/f19_proglangs/jella_vempati/assign4/python@2991 1e9af996-6bc9-4e4b-9a57-10da00564793

--- src/game_of_life.py
+++ src/game_of_life.py
@@ -1,26 +1,16 @@
-#Feedback: let's use 2-space indentation consistenly - fixed it
+from functools import reduce
+import operator
 
-def is_alive(current_status, number_of_live_neighbors): #Feedback: moved this to the top as this is the first function we wrote.
+def is_alive(current_status, number_of_live_neighbors):
   return (number_of_live_neighbors == 2 and current_status) or \
     number_of_live_neighbors == 3
 
-def generate_signals(i):
-    return [(i[0] - 1, i[1] - 1), (i[0] - 1, i[1]), (i[0] - 1, i[1] + 1), 
-            (i[0], i[1] - 1), (i[0], i[1] + 1), 
-            (i[0] + 1, i[1] - 1), (i[0] + 1, i[1]), (i[0] + 1, i[1] + 1)]
-
-#Feedback: is the following better?
-#def generate_signals(live_cell):
-#  (x, y) = live_cell
-#  return [(x - 1, y - 1), (x - 1, y), (x - 1, y + 1),
-#    (x, y - 1), (x , y + 1),
-#    (x + 1, y - 1), (x + 1, y), (x + 1, y + 1)]
-
-#Feedback: Let's remove the following function
-def get_live_neighbours_count(universe, index):
-    valid_signals = filter(lambda x: -1 not in x
-                                     and (x[0] < len(universe))
-                                     and (x[1] < len(universe[0]))
-                           , generate_signals(index))
-    return len(list(filter(lambda x: universe[x[0]][x[1]] == 1
-                           , valid_signals)))
+def generate_signals(live_cell):
+  (x, y) = live_cell
+  return [(x - 1, y - 1), (x - 1, y), (x - 1, y + 1),
+    (x, y - 1), (x , y + 1),
+    (x + 1, y - 1), (x + 1, y), (x + 1, y + 1)]
+	
+def generate_signals_for_all_live_cells(live_cells):
+  return reduce(operator.add, 
+    list(map(lambda x: generate_signals(x), live_cells)))
