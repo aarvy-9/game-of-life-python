65c8e2c634987ac609af9161b60209153276442d
rahulv2901@gmail.com <rahulv2901@gmail.com@1e9af996-6bc9-4e4b-9a57-10da00564793>
2019-11-12 07:00:50 +0000

    main function to print generation
    git-svn-id: https://agile.cs.uh.edu/svn/f19_proglangs/jella_vempati/assign4/python@3102 1e9af996-6bc9-4e4b-9a57-10da00564793

--- src/game_of_life.py
+++ src/game_of_life.py
@@ -1,6 +1,8 @@
 from functools import reduce
 from collections import Counter
 import operator
+from time import sleep
+import os
 
 def is_alive(current_status, number_of_live_neighbors):
   return (number_of_live_neighbors == 2 and current_status) or \
@@ -23,3 +25,14 @@ def next_generation(live_cells):
   signal_count = count_signals(all_live_signals)
   return list(filter(lambda x: is_alive(x in live_cells, signal_count[x]), 
                 signal_count))
+				
+def main():
+  live_cells = [(0, 0), (0, 1), (0, 2)]
+  while True:
+    os.system('cls')
+    print(live_cells)
+    live_cells = next_generation(live_cells)
+    sleep(1)
+	
+main()
+  
